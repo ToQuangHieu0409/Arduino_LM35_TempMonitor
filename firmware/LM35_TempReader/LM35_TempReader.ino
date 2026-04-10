6b4de2b6ce71c43f1172b1255cb3ee6e266f2b3e
Hieu <kid074334@gmail.com>
2026-04-10 14:00:47 +0700

    feat: ho tro doc 2 kenh LM35 (A0, A1)

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -5,15 +5,23 @@
 
 int adcValue;
 float nhietDo;
+int adcValues[2];
+float nhietDo[2];
+char chuoi[30];
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   delay(100);
 }
 
-void loop() {
-  adcValue = analogRead(A0);
-  nhietDo = (adcValue * 500.0) / 1023.0;
-  Serial.println(nhietDo, 1);
-  delay(1000);
+void loop()
+{
+  adcValues[0] = analogRead(A0);
+  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+  adcValues[1] = analogRead(A1);
+  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+  Serial.print(chuoi);
+  delay(100);
 }
