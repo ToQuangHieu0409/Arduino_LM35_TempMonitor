ce62900a40ba995a978afcef82c7ec834c43a87a
Hieu <kid074334@gmail.com>
2026-04-14 15:08:44 +0700

    feat: them kenh A2

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -23,6 +23,8 @@ void loop()
   adcValues[1] = analogRead(A1);
   nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
 
+  adcValues[2] = analogRead(A2);
+  nhietDo[2] = (adcValues[2] * 500.0) / 1023.0;
   sprintf(chuoi, "%d,%d,%d\n", (int)nhietDo[0], (int)nhietDo[1], (int)nhietDo[2]);
   Serial.print(chuoi);
   delay(100);
